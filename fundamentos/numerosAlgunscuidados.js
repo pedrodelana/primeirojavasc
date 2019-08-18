316cc155999e7e2c9e8fd6a40d7e444dcbb1d96c
Pedro Lana <pedroalana@hotmail.com>
2019-08-18 20:22:21 -0300

    Metade do javascript

---
+++ fundamentos/numerosAlgunscuidados.js
@@ -0,0 +1,3 @@
+console.log(7/0.001);
+console.log("show" * 2);
+console.log(0.1 + 0.7);
---
+++ fundamentos/templatestring.js
@@ -0,0 +1,12 @@
+const name = "Rebeca";
+const concatenacao = 'Olá' + name + '!';
+const template = `  
+      Olá
+      ${name}!`
+console.log(concatenacao, template);
+
+console.log(`1+1 = ${1 + 1}`);
+
+const up = texto => texto.toUpperCase();
+
+console.log(`Ei ... ${up('cuidado')}!`);
